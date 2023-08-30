a3a8128bc53abd677fb2195f4ac0a72b4e70c1b6
Harshaan Babra <harshaanniles010@gmail.com>
2023-08-30 11:50:18 -0700

    Update settings.py

--- renisu/settings.py
+++ renisu/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-8b*l^kf#w170jet@jx+g&((4+&3vmik&1o2_^yvkx6*e3u#!&*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
