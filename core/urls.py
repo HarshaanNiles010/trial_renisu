4b57a94f998bab330dbf97eed06304d87f13243d
HarshaanNiles010 <harshaanniles010@gmail.com>
2023-09-29 10:39:44 -0700

    Review fucntionality completed & tested

--- core/urls.py
+++ core/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
      path("category/",views.category_list_view, name = "category-list"),
      path("category/<cid>/",views.category_product_list_view, name = "category-product-list"),
      path("product/<pid>/",views.product_detail_view, name = "product-detail"),
+     path("ajax-add-review/<pid>/", views.ajax_add_review, name = "ajax-add-review" ),
      # This url is only used for work in progress apps
      path('wip/',views.wip_view, name = "wip" )
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from taggit.models import Tag
 from core.models import Product, Category, CartOrder, CartOrderProducts, ProductImages, wishlist_model, Address, ProductReview
+from core.forms import ProductReviewForm
+from django.db.models import Count, Avg
 # Create your views here.
 
 def index(request):
@@ -44,11 +46,21 @@ def product_detail_view(request, pid):
     p_images = product.p_images.all()
     #products = Product.objects.filter(category=product.category).exclude(pid)
     reviews = ProductReview.objects.filter(product=product)
+    review_form = ProductReviewForm()
+    make_review = True
+    
+    if request.user.is_authenticated:
+        user_review_count = ProductReview.objects.filter(user=request.user, product=product).count()
+        if user_review_count > 0:
+            make_review = False
+        
     context = {
         "product":product,
         "p_images":p_images,
         #"products":products,
-        "reviews":reviews
+        "reviews":reviews,
+        "review_form":review_form,
+        "make_review":make_review
     }
     #print(reviews.review)
     return render(request,'core/product-detail.html',context)
@@ -64,3 +76,26 @@ def wip_view(request):
 #        "reviews" : reviews
 #    }
 #    return render(request, '')
+
+def ajax_add_review(request, pid):
+    product = Product.objects.get(pid=pid)
+    user = request.user
+    review = ProductReview.objects.create(
+        user = user,
+        product = product,
+        review = request.POST['review'],
+        rating = request.POST['rating'],
+    )
+    context = {
+        'user':user.username,
+        'review':request.POST['review'],
+        'rating':request.POST['rating']
+    }
+    average_reviews = ProductReview.objects.filter(product=product).aggregate(rating=Avg("rating"))
+    return JsonResponse(
+        {
+            'bool': True,
+            'context': context,
+            'average_reviews': average_reviews,
+        }
+    )
