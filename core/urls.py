1cb1ab1228e8dc206dd2732390e120e153d2f44a
HarshaanNiles010 <harshaanniles010@gmail.com>
2023-10-09 11:40:41 -0700

    delete-update functionality

--- core/urls.py
+++ core/urls.py
@@ -13,6 +13,8 @@ urlpatterns = [
      path("search/", views.search_view, name="search"),
      path("add-to-cart/", views.add_to_cart, name="add-to-cart"),
      path("cart/", views.cart_view, name="cart"),
+     path("delete-from-cart/",views.delete_item_from_cart, name="delete-from-cart"),
+     path("update-cart/",views.update_cart,name="update-cart"),
      # This url is only used for work in progress apps
      path('wip/',views.wip_view, name = "wip" )
 ]
